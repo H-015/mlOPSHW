d8712168d1e610116b84d2904dee6729aff83dec
Даниил <61912430+H-015@users.noreply.github.com>
2024-06-23 21:24:57 +0500

    Create test_model.py

---
+++ HW3/test_model.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import pickle
+
+from lab3.entity import IrisRequest, IrisPredictEntity, IrisPredictResponse
+
+model_path = './model.pkl'
+
+
+def get_model():
+    return pickle.load(open(model_path, 'rb'))
+
+
+def get_predict(model, data: IrisRequest):
+    response_list = list()
+    for el in data.iris_entities:
+        df = pd.DataFrame([[
+            el['sepal_len'],
+            el['sepal_wid'],
+            el['petal_len'],
+            el['petal_wid']]])
+        prediction = model.predict(df)
+
+        response_row = IrisPredictEntity(
+            el['sepal_len'],
+            el['sepal_wid'],
+            el['petal_len'],
+            el['petal_wid'],
+            prediction[0]
+        )
+        response_list.append(response_row)
+    return IrisPredictResponse(response_list)
+0 comments on commit 1e4f80f
